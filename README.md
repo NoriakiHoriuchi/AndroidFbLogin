68fb5046c21bd5ba5e2073e695426cf506c8143b
NoriakiHoriuchi <youpsit@gmail.com>
2014-10-06 03:04:37 +0900

    finish setting up Facebook Login.

---
+++ README.md
@@ -0,0 +1,15 @@
+FbLogin
+
+
+- Android Studio
+    - Gradle
+- Android SDK
+    - Build SDK version: 20
+    - Build tools version: 20
+    - Minimin SDK version: 14
+    - Target SDK version: 20
+- Facebook SDK
+    - version 3.18.1
+
+
+You should fill Facebook App ID in res/values/strings.xml
--- app/src/main/java/me/gizio/fblogin/MyActivity.java
+++ app/src/main/java/me/gizio/fblogin/MyActivity.java
@@ -1,9 +1,17 @@
 package me.gizio.fblogin;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.model.GraphUser;
 
 
 public class MyActivity extends Activity {
@@ -12,6 +20,30 @@ public class MyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        // start Facebook Login
+        Session.openActiveSession(this, true, new Session.StatusCallback() {
+
+            // callback when session changes state
+            @Override
+            public void call(Session session, SessionState state, Exception exception) {
+                if (session.isOpened()) {
+
+                    // make request to the /me API
+                    Request.newMeRequest(session, new Request.GraphUserCallback() {
+
+                        // callback after Graph API response with user object
+                        @Override
+                        public void onCompleted(GraphUser user, Response response) {
+                            if (user != null) {
+                                TextView welcome = (TextView) findViewById(R.id.hello_world);
+                                welcome.setText("Hello " + user.getName() + "!");
+                            }
+                        }
+                    }).executeAsync();
+                }
+            }
+        });
     }
 
 
@@ -35,5 +67,11 @@ public class MyActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
     }
 }
